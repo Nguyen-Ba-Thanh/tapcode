a61557aff414c61e469fb5878cff9f7d1f7842d6
Nguyen Ba Thanh <thanhbait2k5@gmail.com>
2025-07-25 23:12:49 +0700

    test

--- robot.py
+++ robot.py
@@ -1,12 +1,17 @@
+import math
 class robot:
     def __init__(self,x,y):
         self.y=y
         self.x=x
 
-
     def move(self,huongchay):
         self.x=self.x+huongchay[0]
         self.y=self.y+huongchay[1]
+        
+        if abs(self.x)  > 5 or abs( self.y) >5:
+            print("ban da di chuyen ra ngoai pham vi, vui long di lai")
+            return False
+        else: return True
 
     def vitri(self):
         print(f"vi chi hien tai cua ban la: {self.x}, {self.y}")
@@ -15,25 +20,53 @@ class robot:
 
 
 class chay:
+    huongchay=[[0,1],[0,-1],[1,0],[-1,0]]
     def __init__(self,robot,dichuyen):
         self.robot=robot
         self.dichuyen=dichuyen
 
     def setmove(self):
         if self.dichuyen=="TREN":
-            self.robot.move(huongchay[0])
+            self.robot.move(self.huongchay[0])
         if self.dichuyen=="DUOI":
-            self.robot.move(huongchay[1])
+            self.robot.move(self.huongchay[1])
         if self.dichuyen=="TRAI":
-            self.robot.move(huongchay[3])
+            self.robot.move(self.huongchay[3])
         if self.dichuyen=="PHAI":
-            self.robot.move(huongchay[2])      
+            self.robot.move(self.huongchay[2])   
+
+# ke thua
+class smartrobot(robot):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.history=[(x,y)]
+
+    def move(self, huongchay):
+        vitri=[self.x,self.y]
+        super().move(huongchay)
+        vitrimoi=[self.x,self.y]
+        if vitrimoi!=vitri:
+            self.history.append(vitrimoi)
+    def move_history(self):
+        for i in self.history:
+            print(i)
+
+    pass   
 
 
-robot1=robot(0,0)
+# robot1=robot(0,0)
+# lichsu=[]
+robot1=smartrobot(0,0)
 huongchay=[[0,1],[0,-1],[1,0],[-1,0]]
-dichuyen=input(f"chon huong ban muon di chuyen: \n Trai \n Phai \n Tren \n Duoi\n").upper()
-chay1=chay(robot1,dichuyen)
-chay1.setmove()
-robot1.vitri()
+soluot=5
+i=0
+while i < soluot:
+    dichuyen=input(f"chon huong ban muon di chuyen: \n Trai \n Phai \n Tren \n Duoi\n").upper()
+    chay1=chay(robot1,dichuyen)
+    chay1.setmove()
+    robot1.vitri()
+    robot1.move_history()
+    i+=1
+# print(f"{chay.__dict__}")
+
 
